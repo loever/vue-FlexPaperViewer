61e8c999ef8d82f78cc3808c98ea6db37b4b2a20
Ranyvip <46674639+loever@users.noreply.github.com>
2019-05-14 22:04:06 +0800

    vue- FlexPaperViewer
    vue使用FlexPaperViewe 插件  vue 项目预览swf 文件 ，预览pdf文件传swf 文件r

---
+++ vue-flexpaper/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+      <iframe src="flexpaper2\FlexPaperViewer.html" frameborder="0" style="width:850px;height:800px;"></iframe>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
